0aca51adcffd07095aa39d4f67af7fb86b3aee7c
haishengsun <haishengsun@tencent.com>
2017-06-17 17:24:50 +0800

    test2

--- HelloWord/main.cpp
+++ HelloWord/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "haisheng,Hello, World!\n";
     std::cout << "test\n";
+    std::cout << "test2\n";
     return 0;
 }
