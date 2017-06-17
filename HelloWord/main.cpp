be6565505d7c01e0c150c603897eb9bf6254c8e2
haishengsun <haishengsun@tencent.com>
2017-06-17 15:46:57 +0800

    first commit

---
+++ HelloWord/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWord
+//
+//  Created by haishengsun(孙海胜) on 2017/6/17.
+//  Copyright © 2017年 haishengsun(孙海胜). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "haisheng,Hello, World!\n";
+    std::cout << "test\n";
+    return 0;
+}
